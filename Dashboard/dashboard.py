a7d7d750081b5193ddcf76b1ba57e04ea9410376
CyrilAcn <cyril.acien@gmail.com>
2022-09-05 17:19:40 +0200

    Ajout des fichiers de configuration pour le dashboard

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -11,15 +11,16 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import requests
+import joblib
 import shap
 import pickle
 import altair as alt
+import time
 
 
 # Définition des path
-path ='http://127.0.0.1:8000/'
+path ='https://cyrilaciendashboard.herokuapp.com/'
 APP_PRED = 'predict'
-APP_FEAT = 'featimp'
 
 # Liste des ID de clients
 df = pd.read_csv("df.csv")
@@ -62,22 +63,6 @@ def request_prediction(model_pred, data):
     return response.json()
 
 
-# Requests permet d'envoyer des requêtes HTTP en Python.
-# Elle permet donc d'utiliser des API.
-#def request_featimp(model_pred, data):
-
-    #data_json = {'id': data}
-    #Requête sur le seveur Fast api
-    #response = requests.post(path + APP_FEAT, params=data_json)
-
-    #if response.status_code != 200:
-        #raise Exception(
-            #"Request failed with status {}, {}".format(response.status_code, 
-                                                       #response.text))
-
-    #return response.json()
-
-
 def main():
     customer_id = st.sidebar.selectbox(
         "Entrez le numéro d'identification du client",
@@ -86,47 +71,50 @@ def main():
     pos = (df.index.get_loc(customer_id))
     predict_btn = st.sidebar.button('Prédire')
     
-
     
     with st.container():
         st.title('Client')
 
+        if predict_btn:
+            pred = None
+            pred = request_prediction(APP_PRED, customer_id)    
+            st.subheader(
+                    '**Le risque calculé est de {:.2f}%**'.format(pred.get("proba")*100)) 
+            
+            my_bar = st.progress(0)
+            for percent_complete in range(round(pred.get("proba")*100)):
+                time.sleep(0.01)
+                my_bar.progress(percent_complete + 1)
+
+            if pred.get("avis")=="Avis favorable":
+                st.success(
+                'Décision concernant le crédit : **{}**'.format(pred.get("avis"))) 
+            else:
+                st.error(
+                'Décision concernant le crédit : **{}**'.format(pred.get("avis")))
+
         col1, col2 = st.columns(2)
 
         with col1:
-            st.text('Jauge de risque estimé')
+            st.text('Valeurs des variables importantes')
             #predict_btn = st.button('Prédire')
             if predict_btn:
-                pred = None
-                pred = request_prediction(APP_PRED, customer_id)    
-                chart_data = pd.DataFrame( [[1-pred.get("proba"),
-                                             pred.get("proba")]])
-                st.bar_chart(chart_data)
-                st.write(
-                    'Le risque calculé est de {:.2f}'.format(pred.get("proba"))) 
+                df2= df.T
+                st.dataframe(df2[customer_id],height=350)
 
         with col2:
             st.text('Variables importantes pour la prédiction')
             if predict_btn:
-                #feat = None
-                #feat = request_featimp(APP_FEAT, customer_id)
                 shap.plots._waterfall.waterfall_legacy(exp.expected_value[1],
                                                        shap_values[1][pos,:], 
                                                        feature_names=df.columns, 
                                                        max_display=10)
-                plt.pyplot.savefig('waterfall.png')
-                st.write()
+                plt.pyplot.savefig('waterfall.png',bbox_inches="tight")
+                #st.write()
                 #st.write(
                 #        'Retour: {}'.format(feat.get("feature")))
-                st.image('/tmp/waterfall.png')
-    
-        if predict_btn:
-            if pred.get("avis")=="Avis favorable":
-                st.success(
-                'Décision concernant le crédit : {}'.format(pred.get("avis"))) 
-            else:
-                st.error(
-                'Décision concernant le crédit : {}'.format(pred.get("avis"))) 
+                st.image('/tmp/waterfall.png', width=(600))
+     
     
     st.text("__________________________________________________________________________________")
 
@@ -141,11 +129,10 @@ def main():
             
         with col4:
             st.text("Features importance globales")
-            st.image('/tmp/imp_val.png')
+            st.image('/tmp/imp_val.png',width=(600))
 
-    choix2=df.columns.values.tolist()
+    choix2 = df.columns.values.tolist()
     var_sel2 = st.selectbox("Choisissez une variable pour voir sa relation avec la classe des clients", choix2)
-
     c = alt.Chart(scatterplot).mark_circle().encode(
          x=var_sel2, y='Taux de risque')
     st.altair_chart(c, use_container_width=True)            
