99e1ff256812838066cff1d33af904ba055cec15
CyrilAcn <cyril.acien@gmail.com>
2022-09-05 10:45:47 +0200

    Supression de commentaires dans le fichier app.py

--- API/app.py
+++ API/app.py
@@ -58,17 +58,6 @@ def predict_customer(id:int):
         else:
             return{"proba" : prediction[0,1], "avis" : "Avis favorable"}
 
-
-# 5. Fonction de prédiction, réalisation via les données au format
-#   JSON and retourne the probabilité d'appartenance aux deux classes
-#   ainsi qu'un avis
-
-#@app.post('/featimp')
-#def predict_customer(id:int):
-
-    #if id in list_id:
-            #return{"proba" : prediction[0,1], "avis" : "Avis favorable"}
-
 #if __name__ == '__main__':
     #uvicorn.run(app, host='127.0.0.1', port=8000)
 
